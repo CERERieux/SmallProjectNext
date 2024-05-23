181224a51e180d7a47f1a003e81387a5205003ed
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-05-23 15:03:16 -0600

    Move to comments carpet

--- app/comments/Question.tsx
+++ app/comments/Question.tsx
@@ -1,16 +1,17 @@
 import type { QuestionProps } from "../lib/types";
 
-export default function Question({ question, answer = "" }: QuestionProps) {
-  const questionShort =
-    question.length > 28 ? question.slice(0, 28) + "..." : question;
-  const answerShort = answer.length > 28 ? answer.slice(0, 28) + "..." : answer;
+export default function Question({
+  question,
+  answer = "",
+  author,
+}: QuestionProps) {
   return (
     <>
       <button className="btnOpenDialog flex h-20 w-72 flex-col justify-center gap-4 overflow-hidden rounded-2xl bg-slate-50 px-6 text-sm text-black">
         <h3 className="h-5 w-52 overflow-hidden overflow-ellipsis whitespace-pre-wrap text-start">
-          Q: {question}
+          Q: {question} {`By - ${author}`}
         </h3>
-        <p className="h-5 w-52 overflow-hidden overflow-ellipsis whitespace-pre-wrap">
+        <p className="h-5 w-52 overflow-hidden overflow-ellipsis whitespace-pre-wrap text-left">
           A: {answer}
         </p>
       </button>
