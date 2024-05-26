d039c35b506e5e502f49d06aebb38642186979a1
CERERieux <59849778+CERERieux@users.noreply.github.com>
2024-05-25 19:37:57 -0600

    add style and text

--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -11,16 +11,19 @@ export default function CommentsPage({ searchParams }: CommentPageProps) {
   const page = Number(searchParams?.page) || 1;
 
   return (
-    <main className="relative flex h-screen w-screen flex-col items-center gap-4 overflow-y-auto px-8 py-6 dark:bg-neutral-900 dark:text-slate-50">
+    <main className="relative flex h-screen w-screen flex-col items-center gap-4 overflow-y-auto px-8 py-6 dark:bg-black/70 dark:text-slate-50">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] dark:bg-black"></div>
       <h1 className="text-center text-2xl text-sky-500 dark:text-lime-300">
         Comments and Questions
       </h1>
       <div className="flex w-full items-center justify-around">
         <p className="text-sm dark:text-yellow-100">
-          Hi and welcome! Here you can send me any question, comment or in
-          general, share anything you want :) <br />
-          I&apos;ll try to reply when I&apos;m free! <br />
-          (Only the comments I replied to will appear below <s>just in case</s>)
+          Hi and welcome! Here you can send me anything you want to share with
+          me, a question, comments, <s>a bug you found in this webpage</s> :){" "}
+          <br />
+          I&apos;ll try to read it and reply when I&apos;m free! <br />
+          (Only the comments I replied to will appear below, but I can read all
+          that is sent)
         </p>
         <SendComment />
       </div>
