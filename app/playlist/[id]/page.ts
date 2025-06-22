eef86724dd1ec522f8e34a2a3b265728e45552a9
Erik Rieux <59849778+CERERieux@users.noreply.github.com>
2025-06-22 15:44:17 -0600

    Update page.ts
    Remove 2 playlist

--- app/playlist/[id]/page.ts
+++ app/playlist/[id]/page.ts
@@ -8,14 +8,12 @@ const PLAYLISTS = [
   "https://www.youtube.com/playlist?list=PLwMOB9Os2H-EhfJraPlKdwjRG9zUzgIW0",
   "https://www.youtube.com/playlist?list=PLwMOB9Os2H-HAZOY9cYVye0kydZME8hll",
   "https://www.youtube.com/playlist?list=PLwMOB9Os2H-GRbukjARnAZ471I-pzeCCY",
-  "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-  "https://www.youtube.com/playlist?list=PLwMOB9Os2H-Fnh-J6cdWRC9jCkyN60jHE",
 ];
 
 export default function PlaylistPage({ params }: PlaylistPageParams) {
   const { id } = params;
   const playlist = Number(id);
-  if (playlist < 1 || playlist > 5) {
+  if (playlist < 1 || playlist > 3) {
     notFound();
   }
   redirect(PLAYLISTS[playlist - 1]);
